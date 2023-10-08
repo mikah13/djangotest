d358428c1c5a3bd7009d8c1c9ab4b20e4a0e69ab
mikah13 <anhminhhoang13@gmail.com>
2023-10-07 18:43:50 -0700

    added more test

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.views import generic
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.http import Http404, HttpResponseRedirect
-
+from django.utils import timezone
 # Create your views here.
 from django.http import HttpResponse
 from django.urls import reverse
@@ -16,7 +16,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+        :5
+    ]
 
 
 class DetailView(generic.DetailView):
